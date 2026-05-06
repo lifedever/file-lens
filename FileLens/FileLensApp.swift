adb27e3c1424c071f214d5fe8ed054a67f177afc
lifedever <lifedever@gmail.com>
2026-05-06 13:05:37 +0800

    feat: wire ModelContainer with disciplined store path

--- FileLens/FileLensApp.swift
+++ FileLens/FileLensApp.swift
@@ -1,11 +1,27 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct FileLensApp: App {
+    let container: ModelContainer
+
+    init() {
+        do {
+            let bundleID = Bundle.main.bundleIdentifier ?? "com.lifedever.FileLens"
+            let storeURL = try StoreMigration.resolveStoreURL(bundleID: bundleID)
+            let schema = Schema([Workspace.self, Rule.self, Condition.self, FileNode.self, FileTag.self])
+            let config = ModelConfiguration(schema: schema, url: storeURL)
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Failed to init ModelContainer: \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             Text("FileLens")
                 .frame(minWidth: 800, minHeight: 500)
         }
+        .modelContainer(container)
     }
 }
